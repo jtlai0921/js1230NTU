82c6860ce25433382bf1501fd708de18bdfac624
不說 <49492695+jtlai0921@users.noreply.github.com>
2021-01-13 20:20:49 +0800

    1

--- js20210113.js
+++ js20210113.js
@@ -1,17 +1,45 @@
-let num=prompt("請輸入正確正整數","");
-mum=parseInt(num);
-let i=1;
-if(num<=0){
-    alert("錯誤");
+// while (true) {
+//     let num = prompt("請輸入正確正整數", "");
+//     mum = parseInt(num);
+//     let i = 1;
+//     if (num <= 0) {
+//         alert("錯誤");
 
-}
-while(i<num){
-    if(num==i*i){
-        alert("答案:"+i);
-        break;
-    }
-    i++;
-}
-if(num==i){
-    alert("無整數解");
+//     }
+//     break;
+// }
+
+
+// while (i < num) {
+//     if (num == i * i) {
+//         alert("答案:" + i);
+//         break;
+//     }
+//     i++;
+// }
+// if (num == i) {
+//     alert("無整數解");
+// }
+// let n = 1;
+// while (n <= 5) {
+//     n++;
+//     if (n == 4) {
+//         continue;
+
+//     }
+
+//     console.log(n);
+// }
+/*
+for(初始化區塊;boolean;每1圈重複執行區塊)
+ */
+// let n = 1;
+// for (; n <= 5;) {
+//     console.log(n);
+//     n++;
+// }
+
+for (n = 10; n <= 50; n += 2) {
+    console.log(n);
+
 }
